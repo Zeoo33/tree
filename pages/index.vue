a6277382c67d2be59039255697ee2c9d3cf8d7d4
uros.vozel <uros.vozel@gmail.com>
2024-11-28 14:29:31 +0000

    styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,65 @@
-
-
 <template>
-  <div>
-    <button @click="goBack" :disabled="history.length === 0">Go Back</button>
-    <button @click="reset" :disabled="history.length === 0">Reset</button>
+  <div class="p-6 bg-gray-100 min-h-screen text-gray-800">
 
-    <div v-if="currentNode && currentNode.question">
-      <Question
-        :question="currentNode.question"
-        :options="currentNode.options"
-        @option-selected="handleOptionSelected"
-      />
-    </div>
-
-    <div v-if="history.length > 0">
-      <h2>Your Selections:</h2>
-      <ul>
-        <li v-for="(selection, index) in history" :key="index">
-          {{ selection.question }}: {{ selection.label }}
+    <!-- Current Question -->
+    <div v-if="currentNode && currentNode.question" class="mb-6">
+      <h1 class="text-3xl font-bold text-gray-900 mb-4">
+        {{ currentNode.question }}
+      </h1>
+      <ul class="text-base text-gray-700 space-y-2">
+        <li 
+          v-for="(option, index) in currentNode.options" 
+          :key="index" 
+          class="hover:underline cursor-pointer"
+          @click="handleOptionSelected(option)"
+        >
+          {{ option.label }}
         </li>
       </ul>
     </div>
 
+    <!-- Selections History -->
+<div v-if="history.length > 0" class="mb-6">
+  <h2 class="text-xl font-semibold text-gray-900">Your Selections:</h2>
+  <ul class="list-disc pl-5 mt-2 text-gray-700">
+    <li v-for="(selection, index) in history" :key="index">
+      <span>{{ selection.question }}</span> <span class="font-bold text-orange-500">{{ selection.label }}</span> 
+    </li>
+  </ul>
+</div>
+
+
+    <!-- Recommended Products -->
     <div v-if="currentNode && currentNode.skus && currentNode.skus.length > 0">
-      <h2>Recommended Products:</h2>
-      <ul>
+      <h2 class="text-xl font-semibold text-gray-900">Recommended Products:</h2>
+      <ul class="list-disc pl-5 mt-2 text-orange-500">
         <li v-for="sku in currentNode.skus" :key="sku">
-          {{ sku }} 
+          {{ sku }}
         </li>
       </ul>
+    </div>
+
+    <!-- Buttons -->
+    <div class="mt-6 flex gap-4"> 
+      <button 
+        @click="goBack" 
+        :disabled="history.length === 0"
+        class="px-4 py-2 bg-gray-800 text-white rounded disabled:opacity-50 disabled:cursor-not-allowed hover:bg-gray-700 transition cursor-pointer" 
+      >
+        Go Back
+      </button>
+      <button 
+        @click="reset" 
+        :disabled="history.length === 0"
+        class="px-4 py-2 bg-gray-800 text-white rounded disabled:opacity-50 disabled:cursor-not-allowed hover:bg-gray-700 transition cursor-pointer" 
+      >
+        Reset
+      </button>
     </div>
   </div>
 </template>
 
 <script>
-import Question from '~/components/Question.vue';
 import treeData from '~/assets/json/decision-tree.json';
 
 export default {
@@ -46,37 +71,33 @@ export default {
   },
   methods: {
     handleOptionSelected(option) {
-      // Store the selection in history
       this.history.push({
-        question: this.currentNode.question, 
-        label: option.label 
+        question: this.currentNode.question,
+        label: option.label,
       });
 
-      // Move to the next node based on the selection
       this.currentNode = option.next
         ? option.next
         : { skus: option.skus, question: null, options: null };
     },
     goBack() {
       if (this.history.length > 0) {
-        this.history.pop(); // Remove the last selection from history
+        this.history.pop();
 
-        // If history is empty, go back to the root
         if (this.history.length === 0) {
           this.currentNode = treeData;
         } else {
-          // Otherwise, traverse the tree based on the history
           let currentNode = treeData;
-          for (let i = 0; i < this.history.length; i++) {
-            const selection = this.history[i];
-            
+          for (const selection of this.history) {
             if (currentNode && currentNode.options) {
-              const option = currentNode.options.find((opt) => opt.label === selection.label);
+              const option = currentNode.options.find(
+                (opt) => opt.label === selection.label
+              );
               if (option) {
                 currentNode = option.next
                   ? option.next
                   : { skus: option.skus, question: null, options: null };
-              } 
+              }
             }
           }
           this.currentNode = currentNode;
@@ -86,7 +107,7 @@ export default {
     reset() {
       this.currentNode = treeData;
       this.history = [];
-    }
+    },
   },
 };
 </script>
